5ac7f50312e7df4d1e3198c40fd774c13dafe33b
Joan Caceres <joan.caceres@ib.edu.ar>
2023-10-19 23:58:33 +0200

    Improving methods to obtain eigenenergies to be faster

--- ferbo.py
+++ ferbo.py
@@ -1,55 +1,39 @@
 import numpy as np
 from tqdm.notebook import tqdm
-from concurrent.futures import ThreadPoolExecutor
-import copy
 from qutip import Qobj, destroy, tensor, qeye, sigmaz, sigmay
 
 class Ferbo:
-    def __init__(self, Ec, El, Delta, r = 0.05, phi_ext = 0):
+    def __init__(self, Ec, El, Delta, r = 0.05, phi_ext = 0, dimension = 100):
         self.Ec = Ec
         self.El = El
         self.Delta = Delta
         self.r = r
         self.phi_ext = phi_ext
+        self.dimension = dimension
 
-    def charge_number_operator(self, dimension = 100):
-        phi_zpf = (8.0 * self.Ec / self.El) ** 0.25
-        n_operator  = 1j * (destroy(dimension).dag() - destroy(dimension)) / phi_zpf /np.sqrt(2)
-        return n_operator
-    
-    def phase_operator(self, dimension = 100):
-        phi_zpf = (8.0 * self.Ec / self.El) ** 0.25
-        phase_operator = (destroy(dimension).dag() + destroy(dimension)) * phi_zpf/ np.sqrt(2)
-        return phase_operator
+        self.phi_zpf = (8.0 * self.Ec / self.El) ** 0.25
+        self.charge_number_operator = 1j * (destroy(self.dimension).dag() - destroy(self.dimension)) / self.phi_zpf /np.sqrt(2)
+        self.phase_operator = (destroy(self.dimension).dag() + destroy(self.dimension)) * self.phi_zpf/ np.sqrt(2)
+        self.ReZ = (self.phase_operator/2).cosm()*(self.r*self.phase_operator/2).cosm()+self.r*(self.phase_operator/2).sinm()*(self.r*self.phase_operator/2).sinm()
+        self.ImZ = -(self.phase_operator/2).cosm()*(self.r*self.phase_operator/2).sinm()+self.r*(self.phase_operator/2).sinm()*(self.r*self.phase_operator/2).cosm()
 
-    def get_hamiltonian(self, dimension = 100) -> Qobj:
-
-        phi_zpf = (8.0 * self.Ec / self.El) ** 0.25
-        n_operator = 1j * (destroy(dimension).dag() - destroy(dimension)) / phi_zpf /np.sqrt(2)
-        phase_operator = (destroy(dimension).dag() + destroy(dimension)) * phi_zpf/ np.sqrt(2)
-        ReZ = (phase_operator/2).cosm()*(self.r*phase_operator/2).cosm()+self.r*(phase_operator/2).sinm()*(self.r*phase_operator/2).sinm()
-        ImZ = -(phase_operator/2).cosm()*(self.r*phase_operator/2).sinm()+self.r*(phase_operator/2).sinm()*(self.r*phase_operator/2).cosm()
-        
-        delta = phase_operator - self.phi_ext
-        hamiltonian = 4*self.Ec*tensor(n_operator**2,qeye(2)) + 0.5*self.El*tensor((delta)**2,qeye(2)) + self.Delta*(tensor(ReZ,sigmaz())+tensor(ImZ,sigmay()))
+    def get_hamiltonian(self) -> Qobj:
+        delta = self.phase_operator - self.phi_ext
+        hamiltonian = 4*self.Ec*tensor(self.charge_number_operator**2,qeye(2)) + 0.5*self.El*tensor((delta)**2,qeye(2)) + self.Delta*(tensor(self.ReZ,sigmaz())+tensor(self.ImZ,sigmay()))
         return hamiltonian
     
-    def get_eigenenergies(self, dimension = 100, eigvals=0) -> np.ndarray:
-        hamiltonian = self.get_hamiltonian(dimension = dimension)
+    def get_eigenenergies(self, phi_ext, eigvals=0) -> np.ndarray:
+        delta = self.phase_operator - phi_ext
+        hamiltonian = 4*self.Ec*tensor(self.charge_number_operator**2,qeye(2)) + 0.5*self.El*tensor((delta)**2,qeye(2)) + self.Delta*(tensor(self.ReZ,sigmaz())+tensor(self.ImZ,sigmay()))
         eigenenergies = hamiltonian.eigenenergies(eigvals=eigvals)
         return np.real(eigenenergies)
-
-    def get_eigenenergies_vs_phase(self, dimension=100, eigvals=6, phi_ext_array=np.linspace(-2*np.pi, 2*np.pi, 100)) -> np.ndarray:
+    
+    def get_eigenenergies_vs_external_phase(self, eigvals=6, phi_ext_array=np.linspace(-2*np.pi, 2*np.pi, 100)) -> np.ndarray:
         eigenenergies = []
-        
-        def task(phi_ext):
-            local_self = copy.deepcopy(self)
-            local_self.phi_ext = phi_ext
-            return local_self.get_eigenenergies(dimension=dimension, eigvals=eigvals)
-        
-        with ThreadPoolExecutor() as executor:
-            eigenenergies = list(executor.map(task, phi_ext_array))
-            
+        for phi_ext in tqdm(phi_ext_array):
+            delta = self.phase_operator - phi_ext
+            hamiltonian = 4*self.Ec*tensor(self.charge_number_operator**2,qeye(2)) + 0.5*self.El*tensor((delta)**2,qeye(2)) + self.Delta*(tensor(self.ReZ,sigmaz())+tensor(self.ImZ,sigmay()))
+            eigenenergies.append(np.real(hamiltonian.eigenenergies(eigvals=eigvals)))
         return np.array(eigenenergies)
     
     def get_eigenenergies_vs_r(self, dimension = 100, eigvals=6, r_array = np.linspace(0,1,100)) -> np.ndarray:
