15c595dc95438f62a900c5d2e199d212bbe6a990
Joan Caceres <joan.caceres@ib.edu.ar>
2023-12-09 21:10:28 +0100

    Fixing bugs of plot_vs_parameters for when you plot only one image

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import inspect
 import matplotlib.pyplot as plt
+import numpy as np
 
 def filter_args(func, params):
     """ Filtra un diccionario de argumentos para incluir solo los que necesita una función. """
@@ -9,20 +10,24 @@ def filter_args(func, params):
 def plot_vs_parameters(x_values_list, y_values_list, parameter_names, ylabels, titles=None, common_title=None, figsize=None, filename=None, single_plot=False, **kwargs):
     plt.close('all')
     
-    num_plots = len(x_values_list)
+    num_plots = len(x_values_list) if isinstance(x_values_list, list) else x_values_list.ndim
     
     if figsize is None:
-        figsize = (2.5 * num_plots, 6)
+        figsize = (6, 6) if num_plots == 1 else (2.5 * num_plots, 6)
     
     fig, ax = plt.subplots(1, num_plots if not single_plot else 1, figsize=figsize, **kwargs)
     
     if num_plots == 1 or single_plot:
         ax = [ax]
+        x_values_list = np.array([x_values_list])
+        y_values_list = np.array([y_values_list])
+        parameter_names = np.array([parameter_names])
+        ylabels = np.array([ylabels])
+        titles = np.array([titles])
     
     sharey = kwargs.get('sharey', False)
-    
     for i in range(num_plots):
-        ax[0 if single_plot else i].plot(x_values_list[i], y_values_list[i], label=titles[i] if single_plot else None)
+        ax[0 if single_plot else i].plot(x_values_list[i], y_values_list[i])
         
         if not sharey or i == 0:
             ax[0 if single_plot else i].set_ylabel(ylabels[i])
@@ -34,7 +39,7 @@ def plot_vs_parameters(x_values_list, y_values_list, parameter_names, ylabels, t
     
     if single_plot:
         ax[0].set_xlabel(parameter_names[0])
-        ax[0].legend()
+        # ax[0].legend()
         
     if common_title:
         fig.suptitle(common_title)
