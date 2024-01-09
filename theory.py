ccab776d955742641b0fccae0b9bad619edb5264
Joan Caceres <joan.caceres@ib.edu.ar>
2024-01-10 00:20:13 +0100

    Comments

--- theory.py
+++ theory.py
@@ -44,29 +44,29 @@ def eingensystem_hamiltonian(Ej: float, r: float, Ec: float, q: float, Nmax: int
     return np.real(eigvals), eigvecs
 
 
-def andreev_bloch_waves(phi: float, Ej: float, r: float, Ec: float, q: float, N: int) -> np.ndarray:
+# def andreev_bloch_waves(phi: float, Ej: float, r: float, Ec: float, q: float, N: int) -> np.ndarray:
 
-    hamiltonian = bloch_andreev_hamiltonian(Ej = Ej, r = r, Ec = Ec, q = q, N = N)
-    _, eigenvectors = np.linalg.eigh(hamiltonian)
-    # eigenvectors_reshaped = eigenvectors.reshape(2*(N+1), -1, 2)
+#     hamiltonian = bloch_andreev_hamiltonian(Ej = Ej, r = r, Ec = Ec, q = q, N = N)
+#     _, eigenvectors = np.linalg.eigh(hamiltonian)
+#     # eigenvectors_reshaped = eigenvectors.reshape(2*(N+1), -1, 2)
 
-    m_vals = np.arange(-N//2, N//2 + 1)
-    factor = np.exp(1j * m_vals/2 * phi)
+#     m_vals = np.arange(-N//2, N//2 + 1)
+#     factor = np.exp(1j * m_vals/2 * phi)
 
-    bloch_waves = [eigenvector * factor[:,np.newaxis] for eigenvector in eigenvectors_reshaped]
-    bloch_waves = np.array(bloch_waves)
-    return bloch_waves
+#     bloch_waves = [eigenvector * factor[:,np.newaxis] for eigenvector in eigenvectors_reshaped]
+#     bloch_waves = np.array(bloch_waves)
+#     return bloch_waves
 
-def omega_matrix(Ej: float, r: float, Ec: float, q: float, N: int):
-    q_list = np.linspace(0,1/2,100)
-    phi_list = np.linspace(0,4*np.pi, 100)
-    list_of_u_list = []
-    list_of_dudq_list = []
-    product_list = []
-    for phi in phi_list:
-        for q in q_list:
-            andreev_bloch_eigenvectors = andreev_bloch_waves(phi, Ej, r, Ec, q, N)
-            list_of_u_list.append(andreev_bloch_eigenvectors)
-        list_of_u_list = np.array(list_of_u_list)
-        list_of_dudq_list = np.gradient(list_of_u_list,q_list)
+# def omega_matrix(Ej: float, r: float, Ec: float, q: float, N: int):
+#     q_list = np.linspace(0,1/2,100)
+#     phi_list = np.linspace(0,4*np.pi, 100)
+#     list_of_u_list = []
+#     list_of_dudq_list = []
+#     product_list = []
+#     for phi in phi_list:
+#         for q in q_list:
+#             andreev_bloch_eigenvectors = andreev_bloch_waves(phi, Ej, r, Ec, q, N)
+#             list_of_u_list.append(andreev_bloch_eigenvectors)
+#         list_of_u_list = np.array(list_of_u_list)
+#         list_of_dudq_list = np.gradient(list_of_u_list,q_list)
         #multiplicar aca los u con los dudq usando einsum
