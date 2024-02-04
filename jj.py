37cf4760aa3cb3161583960ef2997e0062335906
Joan Caceres <joan.caceres@ib.edu.ar>
2024-02-04 18:30:53 +0100

    Adding the method frequency_array_Cin_ini

--- jj.py
+++ jj.py
@@ -38,6 +38,14 @@ def frequency_array_fit5(params):
      Ljj = 4.1e-9
      return frequency_array_Cgbig_half(N,Cjj,Ljj,Cg,Cg_big,Cin=0,Cout = 0)[1:8]
 
+def frequency_array_fit6(params):
+     Cjj, Cg, Cin = params
+     Cjj = Cjj*1e-15; Cg = Cg*1e-18; Cin = Cin*1e-18
+     #To change the values below each time to fit
+     N = 150
+     Ljj = 4.1e-9
+     return frequency_array_Cin_ini(N,Cjj,Ljj,Cg,Cin,Cout = 0)[1:11]
+
 def resonance_model(j, fp, cg_over_cj):
     N = 150
     return fp * np.sqrt((1-np.cos(j*np.pi/N))/(1-np.cos(j*np.pi/N) + cg_over_cj/2))
@@ -167,6 +175,49 @@ def frequency_array_Cgbig_half(N,Cjj,Ljj,Cg,Cg_big,Cin,Cout):
             matrix[-1,-1] = Cjj + Cout
             matrix[-N//4, -N//4] = 2*Cjj+Cg_big
             matrix[N//4, N//4] = 2*Cjj+Cg_big
+            return matrix
+        
+    def L_inv_matrix(N,Ljj):
+        matrix_diagonals = [(-1/Ljj)*np.ones(N),(2/Ljj)*np.ones(N+1),(-1/Ljj)*np.ones(N)]
+        matrix = diags(matrix_diagonals,[-1,0,1]).toarray()
+        matrix[0,0] = 1/Ljj
+        matrix[-1,-1] = 1/Ljj
+        return matrix
+
+    # Solving C^-1/2 L^-1 C^-1/2 phi = \omega^2 phi
+    eigenvalues_C, eigenvectors_C = eigh(C_matrix(N,Cjj,Cg,Cin,Cout))
+    # epsilon = 1e-100  # Un pequeño valor positivo para reemplazar valores propios negativos o muy pequeños
+    # eigenvalues_C_safe = np.where(eigenvalues_C > epsilon, eigenvalues_C, epsilon)
+    # Lambda_inv_sqrt = np.diag(1 / np.sqrt(eigenvalues_C_safe))
+
+    Lambda_inv_sqrt = np.diag(1 / np.sqrt(eigenvalues_C))
+    C_inv_sqrt = np.dot(eigenvectors_C, np.dot(Lambda_inv_sqrt, eigenvectors_C.T)) # spectral decomposition of C^-1/2
+    matrix_operation = np.dot(C_inv_sqrt, np.dot(L_inv_matrix(N,Ljj), C_inv_sqrt)) # C^-1/2 L^-1 C^-1/2
+    eigvals, eigvecs = eigh(matrix_operation) # eigenvalues and eigvecs of C^-1/2 L^-1 C^-1/2
+
+    return np.sqrt(eigvals)/2/np.pi
+
+def frequency_array_Cin_ini(N,Cjj,Ljj,Cg,Cin,Cout):
+
+    # Eigensolution of the linearized circuit matrix for a Josephson junction chain with the following parameters:
+    # N: number of junctions
+    # Cjj: junction capacitance
+    # Ljj: junction inductance
+    # Cg: ground capacitance
+    # Cin: input capacitance
+    # Cout: output capacitance
+
+    # Return the frequency in Hz.
+
+    # References:
+    #   https://theses.hal.science/tel-01369020
+    #   https://journals.aps.org/prb/abstract/10.1103/PhysRevB.92.104508
+
+    def C_matrix(N,Cjj,Cg,Cin,Cout):
+            matrix_diagonals = [np.ones(N)*(-Cjj),(2*Cjj+Cg)*np.ones(N+1),np.ones(N)*(-Cjj)]
+            matrix = diags(matrix_diagonals,offsets=[-1,0,1]).toarray()
+            matrix[0,0] = Cjj + Cin
+            matrix[-1,-1] = Cjj + Cout
             return matrix
         
     def L_inv_matrix(N,Ljj):
