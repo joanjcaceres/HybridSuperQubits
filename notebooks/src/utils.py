f570649f89181d5132b8aa3627a524c572389adf
Joan Caceres <joan.caceres@ib.edu.ar>
2024-03-16 22:53:15 +0100

    Add method plot_evals_vs_flux to the class FluxoniumManager

--- notebooks/src/utils.py
+++ notebooks/src/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.constants as const
 import scipy.stats
 import scqubits as sq
+import matplotlib.pyplot as plt
 from scipy.optimize import differential_evolution
 
 phi0 = const.h/2/const.e/2/np.pi
@@ -13,8 +14,8 @@ El_per_junction = 35.4
 C0_jja = 53e-18
 
 class FluxoniumManager():
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.optimal_fluxonium = None
 
     def fluxonium_creator(self, params, **kwargs):
         """
@@ -40,9 +41,9 @@ class FluxoniumManager():
         Ec = scipy.stats.hmean([Ec_small_jj, Ec_JJA])
         El = El_per_junction/n_junctions
         Ej = Ej_over_area*small_jj_area
-        return sq.Fluxonium(EC=Ec, EL=El, EJ=Ej, flux=flux, cutoff=120, **kwargs) #Change later to consider both fluxes.
+        return sq.Fluxonium(EC=Ec, EL=El, EJ=Ej, flux=flux, cutoff=50, **kwargs) #Change later to consider both fluxes.
 
-    def _optimizer(self,params):
+    def _Gamma2(self,params):
         fluxonium = self.fluxonium_creator(params)
         
         Gamma2 = fluxonium.t2_effective(
@@ -57,11 +58,34 @@ class FluxoniumManager():
 
         return Gamma2
 
-    def optimal_fluxonium(self, bounds):
+    def minimizer(self, bounds):
         #TODO: Add the bounds as a dictionary to be more understandable.
         bounds_list = list(bounds.values())
-        self.result = differential_evolution(func=self._optimizer,bounds=bounds_list) #optimizing the T2 of the fluxonium.
-        return self.fluxonium_creator(self.result.x)
+        self.result = differential_evolution(func=self._Gamma2,bounds=bounds_list) #optimizing the T2 of the fluxonium.
+        self.optimal_fluxonium = self.fluxonium_creator(self.result.x)
+    
+    def plot_evals_vs_flux(self, resonator_freq, plasma_freq):
+
+        if self.optimal_fluxonium is None:
+            raise RuntimeError("minimizer must be run successfully before plotting.")
+        
+        fluxonium = self.optimal_fluxonium
+
+        flux_array = np.linspace(fluxonium.flux-0.5,fluxonium.flux + 0.5,101)
+        spec = fluxonium.get_spectrum_vs_paramvals(param_name='flux',param_vals=flux_array, subtract_ground=True)
+
+        fig,ax = plt.subplots(1,1)
+        fig.suptitle(f'Ec: {np.round(fluxonium.EC,3)}, El: {np.round(fluxonium.EL,3)}, Ej: {np.round(fluxonium.EJ,3)}')
+        ax.plot(np.linspace(0,1,101),spec.energy_table)
+        ax.plot(flux_array, spec.energy_table[:,0]+resonator_freq*np.ones_like(flux_array), color='k', linestyle='--', label = r'$|1g\rangle$')
+        ax.plot(flux_array, spec.energy_table[:,1]+resonator_freq*np.ones_like(flux_array), color='k',linestyle='dotted', label = r'$|1e\rangle$')
+        ax.plot(flux_array, spec.energy_table[:,0]+plasma_freq*np.ones_like(flux_array), color='k', label = r'$\omega_p/2\pi$')
+        ax.plot(flux_array, spec.energy_table[:,0]+f_temp*np.ones_like(flux_array), color='red',linestyle='dotted', label = r'$f_{temp}$')
+
+        ax.legend()
+        ax.set_xlabel(r'$\Phi_{ext}/\Phi_0$')
+        ax.set_ylabel(r'Energy (GHz)')
+        plt.show()
 
 # def calculation_C_JJA(C0, Cj, n):
 #     n = int(n)
