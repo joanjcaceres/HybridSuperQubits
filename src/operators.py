62909bf78f023105189a6593aee8031d4bc1f9eb
Joan Caceres <joan.caceres@ib.edu.ar>
2025-02-03 11:04:11 +0100

    Add creation and annihilation operators as well as Pauli operators (X, Y, Z) in the operator module

--- src/operators.py
+++ src/operators.py
@@ -2,6 +2,76 @@ from qutip import Qobj
 import scipy.sparse as sp
 import numpy as np
 
+import numpy as np
+
+def destroy(dimension: int) -> np.ndarray:
+    """
+    Returns the annihilation (lowering) operator for a given dimension.
+
+    Parameters
+    ----------
+    dimension : int
+        Dimension of the Hilbert space.
+
+    Returns
+    -------
+    np.ndarray
+        The annihilation operator.
+    """
+    indices = np.arange(1, dimension)
+    data = np.sqrt(indices)
+    return np.diag(data, k=1)
+
+def creation(dimension: int) -> np.ndarray:
+    """
+    Returns the creation (raising) operator for a given dimension.
+
+    Parameters
+    ----------
+    dimension : int
+        Dimension of the Hilbert space.
+
+    Returns
+    -------
+    np.ndarray
+        The creation operator.
+    """
+    return destroy(dimension).T.conj()
+
+def sigma_x() -> np.ndarray:
+    """
+    Returns the Pauli-X (sigma_x) operator.
+
+    Returns
+    -------
+    np.ndarray
+        The Pauli-X operator.
+    """
+    return np.array([[0, 1], [1, 0]], dtype=complex)
+
+def sigma_y() -> np.ndarray:
+    """
+    Returns the Pauli-Y (sigma_y) operator.
+
+    Returns
+    -------
+    np.ndarray
+        The Pauli-Y operator.
+    """
+    return np.array([[0, -1j], [1j, 0]], dtype=complex)
+
+def sigma_z() -> np.ndarray:
+    """
+    Returns the Pauli-Z (sigma_z) operator.
+
+    Returns
+    -------
+    np.ndarray
+        The Pauli-Z operator.
+    """
+    return np.array([[1, 0], [0, -1]], dtype=complex)
+
+###########
 
 def cos_phi(N, phi_ext, m = 1):
     """
