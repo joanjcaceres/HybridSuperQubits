977903e6e8e333c885325c98b6816f02a05ea9c3
Joan Caceres <joan.caceres@ib.edu.ar>
2025-02-03 12:50:16 +0100

    Update the SpectrumData class to handle state tables and tphi, and adjust file write logic

--- src/storage.py
+++ src/storage.py
@@ -11,9 +11,10 @@ class SpectrumData:
         system_params: Dict[str, Any],
         param_name: str = None,
         param_vals: np.ndarray = None,
-        state_table: Union[List[np.ndarray], np.ndarray] = None,
+        state_table: Union[List[Qobj], np.ndarray] = None,
         matrixelem_table: Dict[str, np.ndarray] = None,
-        t1_table: Dict[Tuple[int, int, str], np.ndarray] = None,
+        t1_table: Dict[str, np.ndarray] = None,
+        tphi_table: Dict[Tuple[int, int, str], np.ndarray] = None,
         **kwargs
     ) -> None:
         
@@ -21,9 +22,10 @@ class SpectrumData:
         self.param_name = param_name
         self.param_vals = param_vals
         self.energy_table = energy_table
-        self.state_table = state_table
+        self.state_table = state_table if state_table is not None else []
         self.matrixelem_table = matrixelem_table if matrixelem_table is not None else {}
         self.t1_table = t1_table if t1_table is not None else {}
+        self.tphi_table = tphi_table if tphi_table is not None else {}
         for dataname, data in kwargs.items():
             setattr(self, dataname, data)
 
@@ -31,7 +33,7 @@ class SpectrumData:
         """Subtract ground state energies from spectrum"""
         self.energy_table -= self.energy_table[:, 0]
 
-    def filewrite(self, filename: str, overwrite: bool = True) -> None:
+    def filewrite(self, filename: str, overwrite: bool = False) -> None:
         """Save the SpectrumData to an HDF5 file."""
         
         if not overwrite and os.path.exists(filename):
@@ -43,14 +45,22 @@ class SpectrumData:
             f.create_dataset('param_vals', data=self.param_vals)
             f.create_dataset('param_name', data=np.string_(self.param_name))
             f.create_dataset('system_params', data=np.string_(str(self.system_params)))
-            if self.state_table is not None:
+            
+            if isinstance(self.state_table, list) and isinstance(self.state_table[0], Qobj):
+                state_data = np.array([state.full() for state in self.state_table])
+                f.create_dataset('state_table', data=state_data)
+            else:
                 f.create_dataset('state_table', data=self.state_table)
+                
             if self.matrixelem_table:
                 for key, value in self.matrixelem_table.items():
                     f.create_dataset(f'matrixelem_table/{key}', data=value)
             if self.t1_table:
                 for key, value in self.t1_table.items():
                     f.create_dataset(f't1_table/{key}', data=value)
+            if self.tphi_table:
+                for key, value in self.tphi_table.items():
+                    f.create_dataset(f'tphi_table/{key}', data=value)
 
     @staticmethod
     def read(filename: str) -> 'SpectrumData':
@@ -63,6 +73,7 @@ class SpectrumData:
             state_table = f['state_table'][:] if 'state_table' in f else None
             matrixelem_table = {key: f[f'matrixelem_table/{key}'][:] for key in f['matrixelem_table']} if 'matrixelem_table' in f else {}
             t1_table = {tuple(key.split(',')): f[f't1_table/{key}'][:] for key in f['t1_table']} if 't1_table' in f else {}
+            tphi_table = {tuple(key.split(',')): f[f'tphi_table/{key}'][:] for key in f['tphi_table']} if 'tphi_table' in f else {}
         
         return SpectrumData(
             energy_table=energy_table,
@@ -71,5 +82,6 @@ class SpectrumData:
             param_vals=param_vals,
             state_table=state_table,
             matrixelem_table=matrixelem_table,
-            t1_table=t1_table
+            t1_table=t1_table,
+            tphi_table=tphi_table
         )
