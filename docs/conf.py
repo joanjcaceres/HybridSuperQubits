7ee3be888f51506291966caa23eb8ca1c6359aab
Joan Caceres <joan.caceres@ib.edu.ar>
2025-02-14 21:09:28 +0100

    Update Sphinx configuration to include autodoc, napoleon, and viewcode extensions; change HTML theme to Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,25 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'HybridSuperQubits'
 copyright = '2025, Joan Caceres'
 author = 'Joan Caceres'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))  # Permite que Sphinx encuentre los módulos
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',   # Documentar automáticamente desde docstrings
+    'sphinx.ext.napoleon',  # Soporte para docstrings estilo Google/NumPy
+    'sphinx.ext.viewcode',  # Agrega enlaces al código fuente en la doc
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"  # Tema de Read the Docs
 html_static_path = ['_static']
